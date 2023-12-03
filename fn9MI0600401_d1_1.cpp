b9c64eb9e6d3ca1871c93b9023a35343b8fbb3f6
Enisel Kunch <enisel_12@abv.bg>
2023-12-03 18:37:48 +0200

    task one done

--- fn9MI0600401_d1_1.cpp
+++ fn9MI0600401_d1_1.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
-int main(){
+int findDivisorsCount(int number){
+    int counter = 0; 
+    for(int i = 1; i<= number; i++){
+        if(number % i == 0){
+            counter++;
+        }
+    }
+    return counter;
+}
 
-    std::cout<<"Hello world. This is new branch!";
+bool pairsMatch(int k, int x, int y){
+    if(k*findDivisorsCount(x)*findDivisorsCount(y)==x*y){
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    unsigned int n, k;
+    std::cin >> n >> k;
+    if(n <= 0 && k <= 0){
+        std::cout << "Incorrect input!";
+        return -1;
+    }
+
+    int counter = 0;
+
+    for(int i = 1; i<= n; i++){
+        for(int j = 1; j <= i; j++){
+            if (i>=j)
+            {
+                if (pairsMatch(k,j,i)){
+                    counter++;
+                }
+            }
+        }
+        
+    }
+    std::cout << counter << std::endl;
     return 0;
 }
